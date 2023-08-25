737184dd940b1d152ea453aac2c4300de7864720
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-25 17:40:42 -0300

    feat: add product mapper

---
+++ Ganymede/Data/Map/ProductMap.cs
@@ -0,0 +1,28 @@
+﻿using Ganymede.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ganymede.Data.Map
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasDefaultValueSql("uuid_generate_v4()")
+                .Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(70);
+
+            builder.Property(x => x.Description)
+                .IsRequired();
+
+            builder.Property(x => x.Price)
+                .IsRequired();
+        }
+    }
+}
