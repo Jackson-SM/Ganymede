75c3f2f71c1e750af09436ca83cc2b3a17d35791
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-25 17:40:33 -0300

    feat: add product db context

---
+++ Ganymede/Data/ProductDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Ganymede.Data.Map;
+using Ganymede.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ganymede.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            modelBuilder.HasPostgresExtension("uuid-ossp");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
