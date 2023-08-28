4590b5f2b5dcbdd0c81c4b6103455a54ff6834dc
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-28 17:58:27 -0300

    feat: implement user repository

---
+++ Ganymede/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Ganymede.Data;
+using Ganymede.Entities;
+using Ganymede.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ganymede.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly UserDbContext _dbContext;
+
+        public UserRepository(UserDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User?> GetUserByEmail(string email)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> GetUserById(Guid id)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<User> Register(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+
+            await _dbContext.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
