b91db413b78bd40b8fed9285fb50b78e3783c756
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-28 17:57:55 -0300

    feat: add user map

---
+++ Ganymede/Data/Map/UserMap.cs
@@ -0,0 +1,27 @@
+﻿using Ganymede.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ganymede.Data.Map
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id)
+                .HasDefaultValueSql("uuid_generate_v4()")
+                .Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
+
+            builder.Property(x => x.Name).IsRequired();
+            
+            builder.Property(x => x.Email).IsRequired();
+            
+            builder.Property(x => x.Password).IsRequired();
+
+            builder.HasIndex(x => x.Email).IsUnique();
+        }
+    }
+}
