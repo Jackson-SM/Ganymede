6f9766732b718abbe349ab9dc176c1d937f30935
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-28 17:55:04 -0300

    feat: add user repository

---
+++ Ganymede/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Ganymede.Entities;
+
+namespace Ganymede.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> Register(User user);
+        public Task<User> GetUserById(Guid id);
+        public Task<User?> GetUserByEmail(string email);
+    }
+}
