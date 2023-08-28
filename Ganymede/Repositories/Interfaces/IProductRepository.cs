21866c8a5972833830b90c3170659c58fa7775bd
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-28 17:54:53 -0300

    feat: add product repository

---
+++ Ganymede/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ganymede.Entities;
+
+namespace Ganymede.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        public Task<List<Product>> GetProducts();
+        public Task<Product> GetProductById(Guid id);
+    }
+}
