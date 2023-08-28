9b679f9c75a86b1436f2241aaa8702fb17e998ae
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-28 17:58:10 -0300

    feat: add user db context

---
+++ Ganymede/Data/UserDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Ganymede.Data.Map;
+using Ganymede.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ganymede.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.HasPostgresExtension("uuid-ossp");
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
