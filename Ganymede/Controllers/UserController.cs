8d98690c690bb78acafd84be87802deec45d1e0a
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-28 17:58:41 -0300

    feat: add user repository

--- Ganymede/Controllers/UserController.cs
+++ Ganymede/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ganymede.Entities;
+using Ganymede.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ganymede.Controllers
 {
@@ -6,9 +8,38 @@ namespace Ganymede.Controllers
     [Route("users")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
         {
-            return View();
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> Register([FromBody] User user)
+        {
+            User userExists = await _userRepository.GetUserByEmail(user.Email);
+
+            if(userExists != null)
+            {
+                return Conflict("User already exists");
+            }
+
+            return await _userRepository.Register(user);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(Guid id)
+        {
+            User user = await _userRepository.GetUserById(id); 
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
         }
     }
 }
